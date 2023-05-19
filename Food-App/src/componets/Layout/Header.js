f6f8cf926693cdf284891096caf39eaf2e441efe
Owolabi olusegun <52004465+boarnerges@users.noreply.github.com>
2023-05-19 13:26:01 +0100

    Food Application

---
+++ Food-App/src/componets/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="meals" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
