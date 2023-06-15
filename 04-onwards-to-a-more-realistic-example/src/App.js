d742fd24cfd94d846448a97348294c54afc38e4c
Owolabi olusegun <52004465+boarnerges@users.noreply.github.com>
2023-06-15 12:36:16 +0100

    implemeting dependency

--- 04-onwards-to-a-more-realistic-example/src/App.js
+++ 04-onwards-to-a-more-realistic-example/src/App.js
@@ -28,7 +28,7 @@ function App() {
 
   useEffect(() => {
     fetchTasks();
-  }, []);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
