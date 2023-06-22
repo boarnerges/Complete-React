ea82423ccab602f680427c60282b29a788ef18dc
Owolabi olusegun <52004465+boarnerges@users.noreply.github.com>
2023-06-22 09:36:06 +0100

    still on custom hook

--- 04-onwards-to-a-more-realistic-example/src/App.js
+++ 04-onwards-to-a-more-realistic-example/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
 import useHttp from "./hooks/use-http";
@@ -6,30 +6,23 @@ import useHttp from "./hooks/use-http";
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = (tasksObj) => {
+  const transformTasks = useCallback((tasksObj) => {
     const loadedTasks = [];
 
     for (const taskKey in tasksObj) {
       loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
     }
     setTasks(loadedTasks);
-  };
+  }, []);
 
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp(
-    {
-      url: "https://my-task-334f3-default-rtdb.firebaseio.com/tasks.json",
-    },
-    transformTasks
-  );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTasks);
 
   useEffect(() => {
-    fetchTasks();
-  }, [fetchTasks]);
-
+    fetchTasks(
+      { url: "https://my-task-334f3-default-rtdb.firebaseio.com/tasks.json" },
+      [fetchTasks]
+    );
+  });
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
   };
